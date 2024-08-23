025658486d980d1193e922f7246d92a585042680
aria-abadian <aryaabadian@gmail.com>
2024-08-23 21:15:53 +0330

    dist folder upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { socials } from "../constants";
+import Section from "./Section";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div
+        className="container flex sm:justify-between justify-center items-center
+      gap-10 max-sm:flex-col"
+      >
+        <p className="caption text-n-4 lg:block">
+          © {new Date().getFullYear()}. All rights reserved
+        </p>
+        <p className="caption text-n-4 lg:block"> Created by Aria Abadian</p>
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full
+              transition-colors hover:bg-n-6"
+            >
+              <img src={item.iconUrl} alt={item.title} width={16} height={16} />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
 };
 
 export default Footer;
